56dd08bf7e4f0096d0c6c1a40cb179bbf4e2aa0d
noverlean <noverlin1352@gmail.com>
2025-12-05 16:32:48 +0300

    feat: добавил репозиторий для refresh_token

---
+++ src/main/java/noverlin/fitness/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package noverlin.fitness.repository;
+
+import noverlin.fitness.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String tokenHash);
+}
