7dcf1b4e071b8d2116eaefda4483b765fe72a8f7
noverlean <noverlin1352@gmail.com>
2025-12-05 15:54:33 +0300

    Initial commit

---
+++ src/main/java/noverlin/fitness/FitnessTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package noverlin.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noverlin/fitness/FitnessTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package noverlin.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
