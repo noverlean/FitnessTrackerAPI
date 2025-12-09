9f3b6bc946c0ec9f9f9aeab739f56083a95d4979
noverlean <noverlin1352@gmail.com>
2025-12-09 20:30:08 +0300

    fix: исправлен маппинг у Workout и его dto

--- src/main/java/noverlin/fitness/dto/workout/WorkoutResponse.java
+++ src/main/java/noverlin/fitness/dto/workout/WorkoutResponse.java
@@ -1,9 +1,13 @@
 package noverlin.fitness.dto.workout;
 
+import lombok.Getter;
+import lombok.Setter;
 import noverlin.fitness.model.WorkoutType;
 
 import java.time.Instant;
 
+@Getter
+@Setter
 public class WorkoutResponse {
     Long id;
 
